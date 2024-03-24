3e961c0716ce1ef7e6f4fdfc37a32e699e133d6a
weemanconnor <158524354+weemanconnor@users.noreply.github.com>
2024-03-25 10:43:50 +1300

    Update Assesment_mar11a.ino

--- Assesment_mar11a/Assesment_mar11a.ino
+++ Assesment_mar11a/Assesment_mar11a.ino
@@ -1,19 +1,49 @@
 #include <Wire.h>
 #include <SparkFun_Qwiic_OpenLog_Arduino_Library.h>
+#include <SPI.h>
+#include <SD.h>
+Sd2Card card;
+SdVolume volume;
+SdFile root;
+
 OpenLog sdCard; // create instance 
 const String Bottle_rocket_reading = "SD.sensor_reading_storage";
 
 void setup() {
- Wire.begin();
- sdCard.begin(); //Open connection to OpenLog (no pun intended)
- Serial.begin(9600); //9600bps is used for debug statements
+ Serial.begin(9600);
+  Serial.println("LPS25HB Pressure Sensor Example 1 - Basic Readings");
+  Serial.println();
 
-sdCard.append(Bottle_rocket_reading);
-sdCard.println("bottle rocket readings");
-sdCard.syncFile(); 
+  Wire.begin();
+  sensor.begin(); // Begin links an I2C port and I2C address to the sensor, sets an I2C speed, begins I2C on the main board, and then sets default settings
+
+  if (sensor.isConnected() == false) // The library supports some different error codes such as "DISCONNECTED"
+  {
+    Serial.println("LPS25HB disconnected. Reset the board to try again.");     // Alert the user that the device cannot be reached
+    Serial.println("Are you using the right Wire port and I2C address?");      // Suggest possible fixes
+    Serial.println("See Example2_I2C_Configuration for how to change these."); // Suggest possible fixes
+    Serial.println("");
+    while (1)
+      ;
+  }
+
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+void loop()
+{
+  Serial.print("current preassure: ");
+  Serial.print(sensor.getPressure_hPa());  // getting the tempature reading in hPa
+  sdCard.print(", Current tempature (C degrees): ");
+  Serial.println(sensor.getTemperature_degC()); // geting the tempature in degrees 
 
+    sdCard.print("current preassure: ");
+  sdCard.print(sensor.getPressure_hPa());  // getting the tempature reading in hPa
+  sdCard.print(", Current tempature (C degrees): ");
+  sdCard.println(sensor.getTempature_degC()); // geting the tempature in degrees 
+
+delay(40); // waiting 40 miliseconds so that we can goet a good reading like 16, 14, 11, 9 insted of 16, 16, 16, 16, 14, 14, 14, 14, 14, 14, 
 }
+
+
+
+
