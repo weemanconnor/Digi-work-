15feb6c88bca2b3dc6ea46c609162cf0a3445767
weemanconnor <158524354+weemanconnor@users.noreply.github.com>
2024-05-14 11:58:02 +1200

    Update Assesment_mar11a.ino

--- Assesment_mar11a/Assesment_mar11a.ino
+++ Assesment_mar11a/Assesment_mar11a.ino
@@ -5,50 +5,56 @@
 #include <SD.h>
 #include <SparkFun_LPS25HB_Arduino_Library.h> 
 
-//TEACHER COMMENTS what do these lines of code do?
+//These lines of code create an instance.
+LPS25HB Sensor; 
 Sd2Card card;
 SdVolume volume;
 SdFile root;
-OpenLog sdCard; // create instance 
+OpenLog sdCard;  
 
+const byte tempaturesensor = 3;
+const byte chipinput = 5;
 //TEACHER COMMENTS what about this one?
-const String Bottle_rocket_reading = "SD.sensor_reading_storage";
+const String FILENAME = "SD.sensor_reading_storage_CG";
+//setting up the 
 
-void setup() {
+void setup() 
+{
  Serial.begin(9600);
-  Wire.begin();
-sensor.begin(); 
-//TEACHER COMMENTS shouldn't you set up the sdCard?
+ Sensor.begin(); 
+ sdCard.begin();
+ Wire.begin();
 
- 
+ Serial.println("run Mylog/sdCard append file testing");
+ sdCard.println("run Mylog/sdCard append file testing");
+ sdCard.append(FILENAME);
+ sdCard.syncFile();
+ Serial.println("Done!");
+
  //TEACHER COMMENTS what does this chunk of code do? adjust its error message to something you would say
-  if (sensor.isConnected() == false) // The library supports some different error codes such as "DISCONNECTED"
+  if (Sensor.isConnected() == false) // if tyhe sensor is not connected desplay this error message in the serial monitor
   {
-    Serial.println("LPS25HB disconnected. Reset the board to try again.");     // Alert the user that the device cannot be reached
-    Serial.println("Are you using the right Wire port and I2C address?");      // Suggest possible fixes
-    Serial.println("See Example2_I2C_Configuration for how to change these."); // Suggest possible fixes
-    Serial.println("");
+    Serial.println("Sensor is disconnected plug in and restart.");     
     while (1)
       ;
   }
-//TEACHER COMMENTS any sdcard checks to do?
 
- 
 }
-
 void loop()
 {
+
+
  //TEACHER COMMENTS what does this chunk of code do?
-  Serial.print("current preassure: ");
-  Serial.print(sensor.getPressure_hPa());  // getting the tempature reading in hPa
-  sdCard.print(", Current tempature (C degrees): ");
-  Serial.println(sensor.getTemperature_degC()); // geting the tempature in degrees 
+  Serial.println("current preassure: ");
+  Serial.println(Sensor.getPressure_hPa());  // getting the tempature reading in hPa
+  sdCard.println(", Current tempature (C degrees): ");
+  Serial.println(Sensor.getTemperature_degC()); // geting the tempature in degrees 
 
  //TEACHER COMMENTS what about these? check if they make a file that looks like the csv example on classroom
-    sdCard.print("current preassure: ");
-  sdCard.print(sensor.getPressure_hPa());  // getting the tempature reading in hPa
-  sdCard.print(", Current tempature (C degrees): ");
-  sdCard.println(sensor.getTemperature_degC()); // geting the tempature in degrees 
+    sdCard.println("current preassure: ");
+  sdCard.println(Sensor.getPressure_hPa());  // getting the tempature reading in hPa
+  sdCard.println(", Current tempature (C degrees): ");
+  sdCard.println(Sensor.getTemperature_degC()); // geting the tempature in degrees 
 
 delay(40); // waiting 40 miliseconds so that we can goet a good reading like 16, 14, 11, 9 insted of 16, 16, 16, 16, 14, 14, 14, 14, 14, 14, 
 }
