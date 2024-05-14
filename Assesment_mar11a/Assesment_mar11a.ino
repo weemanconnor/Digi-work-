7c61130bb4b6a49e067ef02cf5f19663b48564d9
weemanconnor <158524354+weemanconnor@users.noreply.github.com>
2024-05-15 09:32:31 +1200

    Update Assesment_mar11a.ino

--- Assesment_mar11a/Assesment_mar11a.ino
+++ Assesment_mar11a/Assesment_mar11a.ino
@@ -51,8 +51,8 @@ void launch (bool launch){
  if (launch){
 
     sdCard.println("current preassure: ");
-  sdCard.println(Sensor.getPressure_hPa());  // getting the tempature reading in hPa
-  sdCard.println(", Current tempature (°C):");
+  sdCard.print(Sensor.getPressure_hPa());  // getting the tempature reading in hPa
+    sdCard.print(",");  
   sdCard.println(Sensor.getTemperature_degC()); // geting the tempature in degrees 
   sdCard.syncFile();
   delay(400); // waiting 40 miliseconds so that we can goet a good reading like 16, 14, 11, 9 insted of 16, 16, 16, 16, 14, 14, 14, 14, 14, 14, 
@@ -61,10 +61,10 @@ void launch (bool launch){
  else {
 
  //TEACHER COMMENTS what does this chunk of code do?
-  Serial.println("current preassure: ");
-  Serial.println(Sensor.getPressure_hPa());  // getting the tempature reading in hPa
-  Serial.println(", Current tempature (°C):");
-  Serial.println(Sensor.getTemperature_degC()); // geting the tempature in degrees 
+  Serial.print("current preassure: ");
+  Serial.print(Sensor.getPressure_hPa());  // getting the tempature reading in hPa
+  Serial.print(", Current tempature (°C):");
+  Serial.print(Sensor.getTemperature_degC()); // geting the tempature in degrees 
 }
 }
 
