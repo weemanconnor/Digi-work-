75d161f9f79f59d737aa3cc8510a360109076242
weemanconnor <158524354+weemanconnor@users.noreply.github.com>
2024-05-16 08:43:10 +1200

    Update Assesment_mar11a.ino

--- Assesment_mar11a/Assesment_mar11a.ino
+++ Assesment_mar11a/Assesment_mar11a.ino
@@ -15,7 +15,7 @@ OpenLog sdCard;
 const byte tempaturesensor = 3;
 const byte chipinput = 5;
 //TEACHER COMMENTS what about this one?
-const String FILENAME = "SD.sensor_reading_storage_CG";
+const String FILENAME = "sensorLogCG.txt";
 //setting up the 
 
 void setup() 
@@ -25,46 +25,40 @@ void setup()
  sdCard.begin();
  Wire.begin();
 
- Serial.println("run Mylog/sdCard append file testing");
- sdCard.println("run Mylog/sdCard append file testing");
+ //Serial.println("run Mylog/sdCard append file testing");
+
  sdCard.append(FILENAME);
  sdCard.syncFile();
  Serial.println("Done!");
+ sdCard.println("pressure , tempature");
+ sdCard.syncFile();
+
 
  //TEACHER COMMENTS what does this chunk of code do? adjust its error message to something you would say
   if (Sensor.isConnected() == false) // if tyhe sensor is not connected desplay this error message in the serial monitor
   {
     Serial.println("Sensor is disconnected plug in and restart.");     
-    while (1)
-      ;
+    while (1);
   }
-
 }
 void loop()
 {
-
- launch(false); // false= debug/true = launch
- delay(5000);
+ launch(true); // false= debug/true = launch
 }
 
 void launch (bool launch){
- if (launch){
+ if (launch == true){
 
-    sdCard.println("current preassure: ");
   sdCard.print(Sensor.getPressure_hPa());  // getting the tempature reading in hPa
     sdCard.print(",");  
   sdCard.println(Sensor.getTemperature_degC()); // geting the tempature in degrees 
   sdCard.syncFile();
-  delay(400); // waiting 40 miliseconds so that we can goet a good reading like 16, 14, 11, 9 insted of 16, 16, 16, 16, 14, 14, 14, 14, 14, 14, 
-}
 
- else {
-
+}else {
  //TEACHER COMMENTS what does this chunk of code do?
-  Serial.print("current preassure: ");
   Serial.print(Sensor.getPressure_hPa());  // getting the tempature reading in hPa
-  Serial.print(", Current tempature (°C):");
-  Serial.print(Sensor.getTemperature_degC()); // geting the tempature in degrees 
+  Serial.print(",");
+  Serial.println(Sensor.getTemperature_degC()); // geting the tempature in degrees  
 }
 }
 
